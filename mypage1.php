07145b7f4faef6885db65555b992cfe738252012
sutattra <97584974+sutattra@users.noreply.github.com>
2022-02-22 21:21:30 +0700

    Create mypage1.php

---
+++ mypage1.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>เว็บไซต์ของ สุทัศน์ตรา พั้วพวง</title>
+  </head>
+  <body>
+    <a href="https://facebook.com"><h1>Facebook</h1></a>
+    <a href="mypage2.php"><h1>Mypage2</h1></a>
+  </body>
+</html>
