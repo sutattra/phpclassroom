b5dfd58c64d46ee7728f1a2036c0b4437019a7fd
sutattra <97584974+sutattra@users.noreply.github.com>
2022-02-23 14:05:17 +0700

    Create front.php

---
+++ front.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("baozi.ttf");
+      }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("baozi.ttf");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">นางสาวสุทัศน์ตรา พั้วพวง เลขที่ 9 ห้อง ม.4/12 เลขประจำตัว 56664</h1>
+    <h1 id="font2">นางสาวสุทัศน์ตรา พั้วพวง เลขที่ 9 ห้อง ม.4/12 เลขประจำตัว 56664</h1>
+  </body>
+</html>
