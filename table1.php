e83bd721a1a42d1807f49def21fedc18be43467d
sutattra <97584974+sutattra@users.noreply.github.com>
2022-03-02 13:10:48 +0700

    Update table1.php

--- table1.php
+++ table1.php
@@ -3,12 +3,16 @@
     <title>การสร้างตาราง</title>
   </head>
   <body>
-    <table> border="1"
+    <table border="1"> 
       <tr>
         <td>คอลัมน์ที่ 1</td>
         <td>คอลัมน์ที่ 2</td>
         <td>คอลัมน์ที่ 3</td>
       </tr>
+      <tr>
+        <td>คอลัมน์ที่ 1</td>
+        <td>คอลัมน์ที่ 2</td>
+        <td>คอลัมน์ที่ 3</td>
     </table>
   </body>
 </html>
