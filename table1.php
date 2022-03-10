40f7d6fd186695b6f4f03becda3167ee7042abd7
sutattra <97584974+sutattra@users.noreply.github.com>
2022-03-10 19:38:23 +0700

    Update table1.php

--- table1.php
+++ table1.php
@@ -3,17 +3,43 @@
     <title>การสร้างตาราง</title>
   </head>
   <body>
-    <table border="1"> 
+    <table border="1">
       <tr>
-        <td>คอลัมน์ที่ 1</td>
-        <td>คอลัมน์ที่ 2</td>
-        <td>คอลัมน์ที่ 3</td>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/FB</th>
       </tr>
       <tr>
-        <td>คอลัมน์ที่ 1</td>
-        <td>คอลัมน์ที่ 2</td>
-        <td>คอลัมน์ที่ 3</td>
-        </tr>
+        <td>1</td>
+        <td>War wanarat</td>
+        <td><img src="https://scontent.fphs4-1.fna.fbcdn.net/v/t39.30808-6/237144312_359037889234926_1572876818727699722_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=agHDL0Q1xZEAX8Cpans&_nc_ht=scontent.fphs4-1.fna&oh=00_AT_fSKEcW1VMyvqb7VkI47wMf2rmoJkdlSEsT7kvrRNkEg&oe=622E2403" width="45px"></td>
+        <td><a href="https://twitter.com/warwanarat?s=20&t=4sC9chE-i1FqRA1GdOKLzg">Twitter</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Yin anan</td>
+        <td><img src="https://cdn.idntimes.com/content-images/community/2021/01/120203861-126742925533915-6124392770919653834-n-94eb65a47aef2c86a7df73a61625b424-743cf0a47f0dd3bad2ead5f5401b7f7b.jpg" width="45px"></td>
+        <td><a href="https://twitter.com/yinyin_anw?s=20&t=4sC9chE-i1FqRA1GdOKLzg">Twitter</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Prom ratchapat</td>
+        <td><img src="https://static.naewna.com/uploads/files2017/images/7(793).jpg" width="45px"></td>
+        <td><a href="https://twitter.com/anotherboytj23?s=20&t=4sC9chE-i1FqRA1GdOKLzg">Twitter</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>Mixxiw sahaphap</td>
+        <td><img src="https://pbs.twimg.com/media/EzRD_d3VcAg44Ht.jpg" width="45px"></td>
+        <td><a href="https://twitter.com/wixxiws?s=20&t=4sC9chE-i1FqRA1GdOKLzg">Twitter</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>Plan rathavit</td>
+        <td><img src="https://i.pinimg.com/474x/75/cd/39/75cd39c25909f34858f6e55dbc6c55e7.jpg" width="45px"></td>
+        <td><a href="https://twitter.com/bplannnnn?s=20&t=4sC9chE-i1FqRA1GdOKLzg">Twitter</a></td>
+      </tr>
     </table>
   </body>
 </html>
